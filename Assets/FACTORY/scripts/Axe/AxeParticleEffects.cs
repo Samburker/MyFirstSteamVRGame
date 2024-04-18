02d0ac5ef577bce22c985fe6062db53beb8eeda5
Sami Huttula <sami.huttula@gmail.com>
2024-04-18 13:05:04 +0300

    particle system for axe

---
+++ Assets/FACTORY/scripts/Axe/AxeParticleEffects.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class AxeParticleEffects : MonoBehaviour
+{
+    public float particleDuration = 0.5f; // Duration in seconds for which particle systems remain active
+
+    private ParticleSystem[] particleSystems; // Array to store the particle systems
+
+    void Start()
+    {
+        // Get all the ParticleSystem components attached to child GameObjects
+        particleSystems = GetComponentsInChildren<ParticleSystem>(true);
+
+        // Disable all particle systems at the start
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.Stop();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision belongs to an object with the tag "Zombie"
+        if (collision.gameObject.CompareTag("Zombie"))
+        {
+            // Enable all particle systems
+            foreach (ParticleSystem ps in particleSystems)
+            {
+                ps.Play();
+            }
+
+            // Call the method to disable particle systems after a delay
+            Invoke("DisableParticleSystems", particleDuration);
+        }
+    }
+
+    // Method to disable particle systems
+    private void DisableParticleSystems()
+    {
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            ps.Stop();
+        }
+    }
+}
