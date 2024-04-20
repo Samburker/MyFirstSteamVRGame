afff5d963c8ac300e71d05a89cde425ceecea75c
Sami Huttula <sami.huttula@gmail.com>
2024-04-20 15:49:23 +0300

    added clock to UI

---
+++ Assets/FACTORY/scripts/Clock.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    public Text timerText; // Reference to the Text element displaying the timer
+
+    public string startTime = "06:00"; // Start time in 24-hour clock format (HH:mm)
+    public string endTime = "18:00"; // End time in 24-hour clock format (HH:mm)
+    public float dayLengthInSeconds = 180f; // Length of the day in seconds
+    private DateTime startTimeDT; // Start time as DateTime
+    private DateTime endTimeDT; // End time as DateTime
+    private float timeElapsed = 0f; // Time elapsed since the start of the day
+
+    private void Start()
+    {
+        // Convert start and end times from string to DateTime
+        startTimeDT = DateTime.ParseExact(startTime, "HH:mm", null);
+        endTimeDT = DateTime.ParseExact(endTime, "HH:mm", null);
+    }
+
+    private void Update()
+    {
+        // Update the timer text
+        if (timeElapsed < dayLengthInSeconds)
+        {
+            // Calculate time elapsed since the start of the day
+            timeElapsed += Time.deltaTime;
+            float ratio = timeElapsed / dayLengthInSeconds;
+            TimeSpan timeToAdd = TimeSpan.FromSeconds(ratio * (endTimeDT - startTimeDT).TotalSeconds);
+            DateTime currentTime = startTimeDT.Add(timeToAdd);
+            timerText.text = currentTime.ToString(@"HH\:mm");
+        }
+        else
+        {
+            timerText.text = endTime;
+            // Load the transition scene when time is up
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
